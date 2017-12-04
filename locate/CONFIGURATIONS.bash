3ed1e09c991410ceceda1dee382c746e3b1e1ffc
unixfoundation <main@unixfoundation.org>
2017-12-03 18:14:21 -0600

    Update documentation for MNTPNT_PATHS in CONFIGURATIONS.sh

--- locate/CONFIGURATIONS.bash
+++ locate/CONFIGURATIONS.bash
@@ -16,13 +16,12 @@ DIR_PATHS_DB_DIR="${HERE}/dirsdb"
 # order
 DB_FILE_NAMES=('container1' 'container2' 'home-data')
 
-# Mountpoint paths: Paths of mounted drives containing files and directories to
-# locate. When an output path (in updatedb) begins with any of these mountpoint
-# paths, the mountpoint path of each created path will be replaced by
-# "{MNTPNT_PATH}" in the database. When a file or directory is then located, the
-# mountpoint paths defined here will be traversed through in place of the
-# "{MNTPNT_PATH}" placeholder in each path containing one until the file or
-# directory is found.
+# Paths of mounted drives to be used by locate
+# When a databse file or directory path begins with "{MNTPNT_PATH}/", these
+# paths will traversed through and replace "{MNTPNT_PATH}" until the file or
+# directory is found. Database paths beginning with "{MNTPNT_PATH}/" are
+# created by beginning a file or directory path in updatedb with one of these
+# paths.
 MNTPNT_PATHS=(
   '/mnt/usb1'
   '/mnt/usb2'
