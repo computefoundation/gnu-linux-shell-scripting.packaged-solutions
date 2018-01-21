dad7a48064db4470ba51dd52617c2090c7e0e272
adrianbartyczak <adrianb@unixfoundation.org>
2018-01-20 23:25:56 -0600

    Update documentation in CONFIGURATIONS.bash

--- locatefile/CONFIGURATIONS.bash
+++ locatefile/CONFIGURATIONS.bash
@@ -7,8 +7,8 @@
 #   Configurations for scripts in this directory
 # 
 # Note:
-#   Variable HERE is defined in the sourcing scripts (locatefile and updatedb)
-#   and contains the path to the current directory.
+#   Variable "HERE" is defined in the sourcing scripts (locatefile and updatedb)
+#   and is assigned the path to the current directory.
 # 
 
 # Directory for file paths database files
@@ -17,16 +17,16 @@ FILE_PATHS_DB_DIR="${HERE}/filesdb"
 # Directory for directory paths database files
 DIR_PATHS_DB_DIR="${HERE}/dirsdb"
 
-# File names to use in both database directories; they will be searched in
-# order
+# File names to use in both database directories; note: these files will be
+# searched in the given order in both databases.
 DB_FILE_NAMES=('mount-drive-1-paths' 'mount-drive-2-paths' 'home-paths')
 
-# Paths of mounted drives to be used by locatefile
+# Paths of mounted drives containing files to be located
 # When a databse file or directory path begins with "{MNTPNT_PATH}/", these
-# paths will traversed through and replace "{MNTPNT_PATH}" until the file or
-# directory is found. Database paths beginning with "{MNTPNT_PATH}/" are
-# created by beginning a file or directory path in updatedb with one of these
-# paths.
+# paths will traversed through in place of the "{MNTPNT_PATH}" placeholder until
+# the file or directory is found. Database paths beginning with "{MNTPNT_PATH}/"
+# are created when a database file/directory output path defined in updatedb is
+# prefixed with one of these paths.
 MNTPNT_PATHS=(
   '/mnt/usb1'
   '/mnt/usb2'
